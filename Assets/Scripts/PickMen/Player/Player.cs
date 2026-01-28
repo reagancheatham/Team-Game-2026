01853cb3555227c84d8630cd5c76676385d1c6cf
reagancheatham <reagan.cheatham@eagles.oc.edu>
2026-01-28 16:04:44 -0600

    Add player folder

---
+++ Assets/Scripts/PickMen/Player/Player.cs
@@ -0,0 +1,22 @@
+using Shears.Input;
+using UnityEngine;
+
+namespace PickMen.Players
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private ManagedInputMap inputMap;
+
+        private IManagedInput moveInput;
+
+        private void Update()
+        {
+
+        }
+
+        private void UpdateMovement()
+        {
+            
+        }
+    }
+}
