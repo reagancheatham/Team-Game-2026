7ee3e4cd375fd74f44038daa3b1f766d59944c91
LasagnaWaffle <judahdcampbell@gmail.com>
2026-02-25 16:55:40 -0600

    Add onion model

---
+++ Assets/Scripts/PickMen/Interaction/Onion.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace PickMen.Interaction
+{
+    public class Onion : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
